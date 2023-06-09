abef8936c2a5489e4716c7aebfc2317ad9eb0bb1
Thota Meghana <116430940+thotameghana@users.noreply.github.com>
2023-06-09 18:52:45 +0530

    Create update element.java

---
+++ update element.java
@@ -0,0 +1,66 @@
+/*
+Implement a program to update an element in the given array
+
+Input Format
+
+size,array elements and element to be updated (old element & new element)
+
+Constraints
+
+no
+
+Output Format
+
+return array after updating
+
+Sample Input 0
+
+5
+1 2 3 4 5
+2
+999
+Sample Output 0
+
+1 999 3 4 5
+Sample Input 1
+
+5
+1 2 3 4 5
+1
+888
+Sample Output 1
+
+888 2 3 4 5
+*/
+Implement a program to update an element in the given array
+
+Input Format
+
+size,array elements and element to be updated (old element & new element)
+
+Constraints
+
+no
+
+Output Format
+
+return array after updating
+
+Sample Input 0
+
+5
+1 2 3 4 5
+2
+999
+Sample Output 0
+
+1 999 3 4 5
+Sample Input 1
+
+5
+1 2 3 4 5
+1
+888
+Sample Output 1
+
+888 2 3 4 5
